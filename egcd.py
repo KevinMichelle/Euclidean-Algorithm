92dca11c97278ef63890ddb4d9adf8c5f5866608
Kevin Michelle Contreras González <michelleglz93@gmail.com>
2014-09-18 07:56:19 -0500

    Update egcd.py

--- egcd.py
+++ egcd.py
@@ -8,15 +8,15 @@ def egcd(a, b):
 	y = (0, 1)
 	while a % b != 0:
 		q = a / b
+		c = a % b
 		nx = x[0] - (x[1] * q)
 		ny = y[0] - (y[1] * q)
 		x = (x[1], nx)
 		y = (y[1], ny)
-		auxiliar = a
 		a = b
-		b = auxiliar % b
-	res = (b, x[1], y[1])
-	print res
+		b = c
+	resultado = (b, x[1], y[1])
+	return resultado
 
 if len(sys.argv) != 3:
 	print 'El numero de argumentos es invalido'
@@ -25,6 +25,7 @@ else:
 		if sys.argv[1] > 1 and sys.argv[2] > 1:
 			a = int(sys.argv[1])
 			b = int(sys.argv[2])
-			egcd(a, b)
+			resultado = egcd(a, b)
+			print resultado
 	else:
 		print 'Todos los argumentos deben ser numeros'
